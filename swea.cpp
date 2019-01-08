97c7e06c614783175f9ef53ef368c8d491405dff
Kangjinwoojwk <kjw03230@naver.com>
2019-01-08 17:46:21 +0900

    ssafy703/problem solve/201901018

---
+++ swea.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
+    {
+        int N;
+        cin>>N;
+        int h;
+        int ans=0;
+        int pre=0;
+        int post=0;
+        int x=0;
+        int up=0;
+        for(int i = 0; i < N; i++){
+          
+        }
+        cout<<'#'<<test_case<<' '<<ans<<'\n';
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
