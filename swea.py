1070549c718267abc3990abdec3b550c2fedda1b
Kangjinwoojwk <kjw03230@naver.com>
2019-01-09 15:48:14 +0900

    ssafy703/jinsu`s odd number solve/20190109

--- swea.py
+++ swea.py
@@ -1,11 +1,22 @@
-T = input()
-while T :
+numbers = []
+i = 0
+while i <=1000000 : 
+    numbers += [0]
+    i += 1
+i = 1
+while i <=1000000 :
+    j = i
+    while j <= 1000000 :
+        numbers[j] += i
+        j += i
+    i += 2
+i = 1
+while i <=999999 :
+    numbers[i + 1] += numbers[i]
+    i += 1
+T = int(input())
+for test_case in range(1, T + 1):
     N = input()
-    ans = 0
-    pre = 0
-    post = 0
-    x = 0
-    h = 0
-    while N :
-        h = input()
-        
+    N1 = N.split(' ')
+    ans = numbers[int(N1[1])] - numbers[int(N1[0])-1]
+    print(f'#{test_case} {ans}')
