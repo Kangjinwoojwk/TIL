1dc2c39151579c15656cd16ab1b74e2fbfac1655
Kangjinwoojwk <kjw03230@naver.com>
2019-02-08 17:52:54 +0900

    ssafy703/class/20190208-1

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-import IPython
-print([]=='0')
-print(0=='0')
-IPython.embed()
-print([]==0)
+def sol(bri):
+    result = 0
+    i = 0
+    j = 0
+    dari = []
+    while i < len(bri[1]):
+        
+
+
+        i += 1
+
+
+    return result
+
+bri = [input(), input(), input()]
+bri[0], bri[2] = bri[2], bri[0]
+ans = sol(bri)
+print(ans)
