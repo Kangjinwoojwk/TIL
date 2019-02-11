ff2f3a7501b0ec31083848ff09056885765280be
Kangjinwoojwk <kjw03230@naver.com>
2019-02-11 17:53:36 +0900

    ssafy703/class/20190211

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-bri = [input(), input(), input()]
-bri[0], bri[2] = bri[2], bri[0]
-count = []
-count.append([1,1])
-for i in bri[2]:
-    count.append([0,0])
-for idx1, v in enumerate(bri[0]):
-    for idx2, i in enumerate(bri[2]):
-        if bri[0][idx1] == i:
-            count[idx2 + 1][1] += count[idx2][0]
-        if bri[1][idx1] == i:
-            count[idx2 + 1][0] += count[idx2][1]
-print(sum(count[-1]))
+# bri = [input(), input(), input()]
+# bri[0], bri[2] = bri[2], bri[0]
+# dp = []
+# for i in bri[2]:
+#     dp.append([0,0])
+# dp.append([1,1])
+# for i in range(len(bri[0])):
+N=list(map(int, input().split(' ')))
+result=0
+if N[0]>2*N[1]:
+    if (N[0] - 2 * N[1])>N[2]:
+        print(N[1])
+    else:
+        
