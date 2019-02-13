436b0f81981f770ed563b8c58e7828d9ceb1b059
Kangjinwoojwk <kjw03230@naver.com>
2019-02-13 18:01:44 +0900

    ssafy703/class/20190213

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-# N=int(input())
-# for i in range(N):
-#     for j in range(N):
-#         if 
-
-#         else:
-#             print('*')
-#     print()
-N=list(map(int,input().split()))
+N=int(input())
+get=[]
+for i in range(N):
+    get.append(input())
+ans = 0
+for i in get:
+    chk=''
+    chk1=set()
+    for j in i:
+        if (chk!=j) and (j not in chk1):
+            if chk=='':
+                pass
+            else:
+                chk1.add(chk)
+            chk=j
+        elif chk==j:
+            pass
+        else:
+            break
+    else:
+        ans += 1
+print(ans)
