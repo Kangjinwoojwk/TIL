9c3af75693fe6430b6f668444e538e4fcd2e2b9e
kangjinwoo <kjw03230@naver.com>
2019-02-18 17:47:54 +0900

    ssafy703/class/20190218

--- test.py
+++ test.py
@@ -1,25 +1,6 @@
-star = [
-    '*',
-    '* *',
-    '*****'
-]
-space = [
-    ' ',
-    '   ',
-    '     '
-]
-def hall(n):
-    if n==1:
-        return 5
-    else:
-        return 2 * hall(n - 1) + 1
-N=24#int(input())
-for i in range(N):
-    print(' ' * (N - 1 - i), end='')
-    for j in range((i // 3) + 1):
-        if (i//3)-1==j:
-            print(space[i % 3], end='')
-        else:
-            print(star[i % 3], end='')
-        print(' '*(5 - 2 * (i%3)), end='')
-    print()
+ans = [0]
+def sol(ans):
+    ans[0] += 1
+sol(ans)
+sol(ans)
+print(ans)
--- test1.py
+++
@@ -1,9 +0,0 @@
-N = 24#int(input())
-star = [
-    '*',
-    '* *',
-    '*****'
-]
-result = [''] * N
-for i in range(N):
-    result += ' '*
